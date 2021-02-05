b17f251be9f9533bf662337be005474f1d9d7edb
Arun Prasath <arunprasath218@gmail.com>
2021-02-05 23:24:09 +0800

    Revert "Update home.php"
    This reverts commit 172bcd31077c27e40366960e37566eef5208578e.

--- customer/home.php
+++ customer/home.php
@@ -56,41 +56,6 @@
 				<span class="sr-only">Next</span>
 			</a>
 		</div>
-		
-		<!--Testing-->
-		<div id="myCarousel" class="carousel slide" data-ride="carousel">
-		  <!-- Indicators -->
-		  <ol class="carousel-indicators">
-			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-			<li data-target="#myCarousel" data-slide-to="1"></li>
-			<li data-target="#myCarousel" data-slide-to="2"></li>
-		  </ol>
-
-		  <!-- Wrapper for slides -->
-		  <div class="carousel-inner">
-			<div class="item active">
-			  <img src="../file_store/announcement/banner1.jpg" alt="Los Angeles">
-			</div>
-
-			<div class="item">
-			  <img src="../file_store/announcement/banner2.jpg" alt="Chicago">
-			</div>
-
-			<div class="item">
-			  <img src="../file_store/announcement/banner3.jpg" alt="New York">
-			</div>
-		  </div>
-
-		  <!-- Left and right controls -->
-		  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left"></span>
-			<span class="sr-only">Previous</span>
-		  </a>
-		  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right"></span>
-			<span class="sr-only">Next</span>
-		  </a>
-		</div>
 		<!--End-->
 		
 		<br><br><br>
