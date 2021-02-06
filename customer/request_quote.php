12cb9f4b485e657b46b2a343b00b1a873bde5174
Codinyoyo <rasydanwork@gmail.com>
2021-02-06 17:03:29 +0800

    Update request_quote.php

--- customer/request_quote.php
+++ customer/request_quote.php
@@ -190,7 +190,7 @@
 						</div>
 						
 						<div class="container-fluid">
-							<div style="padding-left:1700px">
+							<div style="padding-left:1670px">
 							<input type="submit" name="submit" class="btn btn-primary float-center btn-sm">
 							<a href="home.php" class="btn btn-info btn-sm float-center" role="button">Back</a>
 							</div>
