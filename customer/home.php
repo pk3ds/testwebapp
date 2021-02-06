5705079cc6373dff9997b32eac1a7bb5f8b80d77
Arun Prasath <arunprasath218@gmail.com>
2021-02-06 17:10:22 +0800

    Update home.php

--- customer/home.php
+++ customer/home.php
@@ -39,10 +39,10 @@
 			
 				<?php while($row = mysqli_fetch_assoc($result)):?>
 				
-				<div class="carousel-item active<?php if ($currentNum == $row){
-					echo active
+				<div class="carousel-item<?php if ($currentNum == $row){
+					echo active;
 				}else{
-					$currentNum++
+					$currentNum++;
 				}?>">
 					<!--Banner is a visible on all screen size device.-->
 					<img class="d-block w-100" src="../file_store/announcement/<?php echo $row["File"]; ?>" alt="banner">
