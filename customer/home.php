59d628247f1d614d89dc6a0d0dc572d3fb07c5b7
Arun Prasath <arunprasath218@gmail.com>
2021-02-06 17:17:16 +0800

    Update home.php

--- customer/home.php
+++ customer/home.php
@@ -7,7 +7,6 @@
 	
 	$sql = "SELECT * FROM announcement WHERE Status = 'Accepted' AND CURRENT_TIMESTAMP < End_Date ";
 	$result = mysqli_query($conn, $sql);
-	$currentNum = 0;
 	if (mysqli_num_rows($result) > 0)
 		
 ?>
@@ -40,7 +39,7 @@
 				<?php while($row = mysqli_fetch_assoc($result)):?>
 				
 				<div class="carousel-item <?php if ($row == 1){
-					echo "active";
+					echo active;
 				}?>">
 					<!--Banner is a visible on all screen size device.-->
 					<img class="d-block w-100" src="../file_store/announcement/<?php echo $row["File"]; ?>" alt="banner">
